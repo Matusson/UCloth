ce2c7c5153c5c61ec4bc6f8c7eacd986ba085812
Matusson <matusson.6105@gmail.com>
2023-02-07 11:38:29 +0100

    Add constructor for UCPointQueryData

--- Runtime/Data/UCPointQueryData.cs
+++ Runtime/Data/UCPointQueryData.cs
@@ -13,8 +13,15 @@ namespace UCloth
         public float3 position;
 
         /// <summary>
-        /// Radius for point search.
+        /// Radius for point search. Zero will pick one point.
         /// </summary>
         public float radius;
+
+
+        public UCPointQueryData(float3 position, float radius = 0)
+        {
+            this.position = position;
+            this.radius = radius;
+        }
     }
 }
